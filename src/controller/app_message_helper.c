6caa01c6f34d55590c50ebef9251ae183bce1c25
Jia Hao <jiahaog@gmail.com>
2015-06-12 11:41:18 +0800

    fixed bug to send string

--- src/controller/app_message_helper.c
+++ src/controller/app_message_helper.c
@@ -3,13 +3,14 @@
 
 // helper method to parse string into an int and send it as an appmessage
 void send_app_message_string(int key, char *message) {
+    // APP_LOG(APP_LOG_LEVEL_DEBUG, "Current service : %s, atoi: %i", message, atoi(message));
     // Begin dictionary
     DictionaryIterator *iter;
     app_message_outbox_begin(&iter);
 
     // Add a key-value pair
     // parse the message to a int at the same time
-    dict_write_uint8(iter, key, atoi(message));
+    dict_write_cstring(iter, key, message);
 
     // Send the message!
     app_message_outbox_send();
