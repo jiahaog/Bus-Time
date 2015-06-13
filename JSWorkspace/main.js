3300aad469103503adcda07db0d26076398e8e9b
Jia Hao <jiahaog@gmail.com>
2015-06-14 02:23:34 +0800

    renamed functions

--- JSWorkspace/main.js
+++ JSWorkspace/main.js
@@ -15,7 +15,7 @@ var lastStopID;
 /**
  * Gets the location of the watch and sends nearby bus stops to the watch
  */
-function processLocation() {
+function sendBusStopList() {
     pebbleHelpers.getLocation(function (error, position) {
         if (error) {
             console.log('location error (' + error.code + '): ' + error.message);
@@ -71,76 +71,69 @@ function sendErrorCode(code) {
             // callback
         }
     });
-
 }
 
-var busTimings = {
+// gets a list of services at the bus stop
+function sendServicesList(stopId) {
+    recordCache.getBusTimings(stopId, undefined, function (error, record) {
+        if (error) {
+            console.log('Error getting bus timings');
+            sendErrorCode(constants.ERROR_CODES.NETWORK_ERROR);
+        } else {
 
-    // gets a list of services at the bus stop
-    sendServicesList: function (stopId) {
-        recordCache.getBusTimings(stopId, undefined, function (error, record) {
-            if (error) {
-                console.log('Error getting bus timings');
-                sendErrorCode(constants.ERROR_CODES.NETWORK_ERROR);
+            const serviceList = recordParser.parseForServicesList(record);
+
+            if (serviceList.length > 0) {
+                pebbleHelpers.sendMessageStream(
+                    constants.APP_MESSAGE_KEYS.KEY_BUS_SERVICE_LIST_START,
+                    constants.APP_MESSAGE_KEYS.KEY_BUS_SERVICE_LIST_VALUE,
+                    constants.APP_MESSAGE_KEYS.KEY_BUS_SERVICE_LIST_END,
+                    serviceList
+                );
             } else {
-
-                const serviceList = recordParser.parseForServicesList(record);
-
-                if (serviceList.length > 0) {
-                    pebbleHelpers.sendMessageStream(
-                        constants.APP_MESSAGE_KEYS.KEY_BUS_SERVICE_LIST_START,
-                        constants.APP_MESSAGE_KEYS.KEY_BUS_SERVICE_LIST_VALUE,
-                        constants.APP_MESSAGE_KEYS.KEY_BUS_SERVICE_LIST_END,
-                        serviceList
-                    );
-                } else {
-                    sendErrorCode(constants.ERROR_CODES.NO_SERVICES_OPERATIONAL);
-                }
-
-
-
+                sendErrorCode(constants.ERROR_CODES.NO_SERVICES_OPERATIONAL);
             }
-        });
-    },
+        }
+    });
+}
 
-    // gets the details for the service
-    sendServiceDetails: function (stopId, serviceNo) {
-        recordCache.getBusTimings(stopId, serviceNo, function(error, record) {
-            if (error) {
-                console.log('Error getting bus timings');
-                sendErrorCode(constants.ERROR_CODES.NETWORK_ERROR);
+function sendServiceDetails(stopId, serviceNo) {
+    recordCache.getBusTimings(stopId, serviceNo, function(error, record) {
+        if (error) {
+            console.log('Error getting bus timings');
+            sendErrorCode(constants.ERROR_CODES.NETWORK_ERROR);
+        } else {
+            //console.log(JSON.stringify(record));
+            const serviceDetails = recordParser.parseForServiceDetails(record, serviceNo);
+
+            var messageString;
+            if (serviceDetails) {
+                messageString =
+                    'Service ' + serviceDetails[constants.RESPONSE_KEYS.serviceNo] + '\n' +
+                    'Next Bus:\n' +
+                    serviceDetails[constants.RESPONSE_KEYS.nextBus][constants.RESPONSE_KEYS.estimatedArrival] + '\n' +
+                    serviceDetails[constants.RESPONSE_KEYS.nextBus][constants.RESPONSE_KEYS.load] + '\n' +
+                    'Subsequent Bus:\n' +
+                    serviceDetails[constants.RESPONSE_KEYS.subsequentBus][constants.RESPONSE_KEYS.estimatedArrival] + '\n' +
+                    serviceDetails[constants.RESPONSE_KEYS.subsequentBus][constants.RESPONSE_KEYS.load];
             } else {
-                //console.log(JSON.stringify(record));
-                const serviceDetails = recordParser.parseForServiceDetails(record, serviceNo);
-
-                var messageString;
-                if (serviceDetails) {
-                    messageString =
-                        'Service ' + serviceDetails[constants.RESPONSE_KEYS.serviceNo] + '\n' +
-                        'Next Bus:\n' +
-                        serviceDetails[constants.RESPONSE_KEYS.nextBus][constants.RESPONSE_KEYS.estimatedArrival] + '\n' +
-                        serviceDetails[constants.RESPONSE_KEYS.nextBus][constants.RESPONSE_KEYS.load] + '\n' +
-                        'Subsequent Bus:\n' +
-                        serviceDetails[constants.RESPONSE_KEYS.subsequentBus][constants.RESPONSE_KEYS.estimatedArrival] + '\n' +
-                        serviceDetails[constants.RESPONSE_KEYS.subsequentBus][constants.RESPONSE_KEYS.load];
-                } else {
-                    messageString = 'Service not found'
-                }
-
-                var dictionaryMessage = {};
-                dictionaryMessage[constants.APP_MESSAGE_KEYS.KEY_BUS_SERVICE_DETAILS_VALUE] = messageString;
-
-                pebbleHelpers.sendMessage(dictionaryMessage, function (error) {
-                    if (error) {
-                        console.log('Error sending message!' + error);
-                    } else {
-                        // callback
-                    }
-                });
+                messageString = 'Service not found'
             }
-        })
-    }
-};
+
+            var dictionaryMessage = {};
+            dictionaryMessage[constants.APP_MESSAGE_KEYS.KEY_BUS_SERVICE_DETAILS_VALUE] = messageString;
+
+            pebbleHelpers.sendMessage(dictionaryMessage, function (error) {
+                if (error) {
+                    console.log('Error sending message!' + error);
+                } else {
+                    // callback
+                }
+            });
+        }
+    })
+}
+
 
 // when the app is launched get the location and send nearby bus stops to the watch
 pebbleHelpers.addEventListener.onReady(function () {
@@ -148,7 +141,7 @@ pebbleHelpers.addEventListener.onReady(function () {
 
     recordCache.restoreCache();
 
-    processLocation();
+    sendBusStopList();
     //setTimeout(function () {
     //    pebbleHelpers.sendNotification('message title', 'message text');
     //}, 10000);
@@ -172,11 +165,11 @@ function processReceivedMessage(event) {
                 var stopId = lastBusStopsIDsSent[value];
                 console.log('Received request for service list for stopID: ' + stopId);
                 lastStopID = stopId;
-                busTimings.sendServicesList(stopId);
+                sendServicesList(stopId);
 
             } else if (key === constants.APP_MESSAGE_KEYS.KEY_BUS_SERVICE_DETAILS_START) {
                 console.log('Received request for service: ' + value);
-                busTimings.sendServiceDetails(lastStopID, value);
+                sendServiceDetails(lastStopID, value);
             }
         }
     }
