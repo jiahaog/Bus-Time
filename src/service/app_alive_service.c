6037c4da1920c34b193c74a7d8e345c64a31ac81
Jia Hao <jiahaog@gmail.com>
2015-07-01 15:37:26 +0800

    Commented out everything

--- src/service/app_alive_service.c
+++ src/service/app_alive_service.c
@@ -1,26 +1,26 @@
-#define APP_ALIVE_SERVICE_REFRESH 300000 // 5 min (300000)
-#define LAST_APP_ALIVE_RESPONSE_THRESHOLD
-#include "app_alive_service.h"
+// #define APP_ALIVE_SERVICE_REFRESH 300000 // 5 min (300000)
+// #define LAST_APP_ALIVE_RESPONSE_THRESHOLD
+// #include "app_alive_service.h"
 
-AppTimer *s_app_alive_service_timer;
+// AppTimer *s_app_alive_service_timer;
 
-static void app_alive_callback() {
-    send_app_message_int(KEY_APP_ALIVE, 1);
+// static void app_alive_callback() {
+//     send_app_message_int(KEY_APP_ALIVE, 1);
 
-    if (get_time_since_last_app_alive_message() > APP_ALIVE_SERVICE_REFRESH/1000) {
-        window_stack_pop_all(false);
-        error_window_push((char*)"Lost connection with JS, reopen the app to continue");
-    } else {
-        s_app_alive_service_timer = app_timer_register(APP_ALIVE_SERVICE_REFRESH, app_alive_callback, NULL);
-    }
-}
+//     if (get_time_since_last_app_alive_message() > APP_ALIVE_SERVICE_REFRESH/1000) {
+//         window_stack_pop_all(false);
+//         error_window_push((char*)"Lost connection with JS, reopen the app to continue");
+//     } else {
+//         s_app_alive_service_timer = app_timer_register(APP_ALIVE_SERVICE_REFRESH, app_alive_callback, NULL);
+//     }
+// }
 
-// starts service to send a message to the watch every 5 min
-void start_app_alive_service() {
-    send_app_message_int(KEY_APP_ALIVE, 1);
-    s_app_alive_service_timer = app_timer_register(APP_ALIVE_SERVICE_REFRESH, app_alive_callback, NULL);
-}
+// // starts service to send a message to the watch every 5 min
+// void start_app_alive_service() {
+//     send_app_message_int(KEY_APP_ALIVE, 1);
+//     s_app_alive_service_timer = app_timer_register(APP_ALIVE_SERVICE_REFRESH, app_alive_callback, NULL);
+// }
 
-void cancel_app_alive_service() {
-    app_timer_cancel(s_app_alive_service_timer);
-}
+// void cancel_app_alive_service() {
+//     app_timer_cancel(s_app_alive_service_timer);
+// }
--- src/service/app_alive_service.h
+++ src/service/app_alive_service.h
@@ -1,10 +1,10 @@
-#pragma once
+// #pragma once
 
-#include <pebble.h>
+// #include <pebble.h>
 
-#include "../controller/app_message_helper.h"
-#include "../model/store.h"
-#include "../windows/error_window.h"
+// #include "../controller/app_message_helper.h"
+// #include "../model/store.h"
+// #include "../windows/error_window.h"
 
-void start_app_alive_service();
-void cancel_app_alive_service();
+// void start_app_alive_service();
+// void cancel_app_alive_service();
