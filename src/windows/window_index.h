9d3754bbc1492c8d53aadca8054f7bd19c870334
Jia Hao <jiahaog@gmail.com>
2015-07-03 16:48:48 +0800

    Added an index of window definitions

---
+++ src/windows/window_index.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+// constants so we don't have to use ints to track the windows
+
+#define BUS_STOPS_WINDOW 0
+#define SERVICES_WINDOW 1
+#define DETAILS_WINDOW 2
+#define ACTION_MENU_WINDOW 3
+#define ERROR_WINDOW 4
+#define NO_WINDOW 999
