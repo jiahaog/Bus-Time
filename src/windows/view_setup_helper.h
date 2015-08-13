c5aae11b759db08fc5bcae72a3925ff0e365b520
Jia Hao <jiahaog@gmail.com>
2015-08-13 12:18:42 +0800

    Change color for aplite and basalt compatibility

--- src/windows/view_setup_helper.h
+++ src/windows/view_setup_helper.h
@@ -7,14 +7,19 @@
 #define COLOR_SECONDARY GColorBulgarianRose
 #define COLOR_TEXT GColorWhite
 
-#define NO_COLOR_BACKGROUND GColorBlack
+#define NO_COLOR_BACKGROUND GColorWhite
 #define NO_COLOR_SECONDARY GColorClear
-#define NO_COLOR_TEXT GColorWhite
+#define NO_COLOR_TEXT GColorBlack
 
 #define FONT_BASIC_TEXT FONT_KEY_GOTHIC_24
 
 #define CELL_H1_FONT FONT_KEY_GOTHIC_24_BOLD
-#define CELL_TEXT_COLOR GColorWhite
+
+#ifdef PBL_COLOR
+    #define CELL_TEXT_COLOR GColorWhite
+#else
+    #define CELL_TEXT_COLOR GColorBlack
+#endif
 
 void menu_layer_set_up(MenuLayer* menu);
 void window_set_up(Window* window);
