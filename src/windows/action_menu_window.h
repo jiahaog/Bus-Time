9df6eed1f1e2ea6117b23ef920c7505988632531
Jia Hao <jiahaog@gmail.com>
2015-08-27 19:07:10 +0800

    Add documentation explaining usage

--- src/windows/action_menu_window.h
+++ src/windows/action_menu_window.h
@@ -5,4 +5,6 @@
 #include "view_setup_helper.h"
 #include "../service/bus_arrival_notifier.h"
 
+// Fallback simple menu layer for APLITE, and is used to set alerts for bus arrival
+
 void action_menu_window_push();
